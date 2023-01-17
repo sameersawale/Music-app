06d90241dbee654728a66e5a6a7487963ab95c19
sameersawale <60250589+sameersawale@users.noreply.github.com>
2023-01-17 15:36:32 +0530

    Music app

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,5 +22,117 @@ public class Main {
 
         LinkedList<Song> myPlayList=new LinkedList<>();
         album1.addToPlaylistFromAlbum("295", myPlayList);
+        album2.addToPlaylistFromAlbum(2,myPlayList);
+        album1.addToPlaylistFromAlbum("Last Ride",myPlayList);
+        album2.addToPlaylistFromAlbum("Hawayein",myPlayList);
+
+        play(myPlayList);
     }
+    public static void play(LinkedList<Song>playList) {
+        ListIterator<Song> itr = playList.listIterator();
+        Scanner sc = new Scanner(System.in);
+        boolean isForward=false;
+
+        if(playList.size()>0){
+            System.out.println("Currently playing: ");
+            System.out.println(itr.next());
+            isForward=true;
+        }
+        else {
+            System.out.println("Playlist is empty");
+            return;
+        }
+        System.out.println("Enter your choice: ");
+        printMenu();
+
+        boolean quite=false;
+
+        while(!quite){
+            int choice= sc.nextInt();
+
+            switch (choice){
+                case 1:
+                    if(isForward==false){
+                        itr.next();
+                        isForward=true;
+                    }
+                    if(itr.hasNext()){
+                        System.out.println(itr.next());
+                        isForward=true;
+                    }
+                    else {
+                        System.out.println("You have reached the end of playlist");
+                        isForward=false;
+                    }
+                    break;
+                case 2:
+                    if(isForward==true){
+                        itr.previous();
+                        isForward=false;
+                    }
+                    if(itr.hasPrevious()){
+                        System.out.println(itr.previous());
+                        isForward=false;
+                    }
+                    else {
+                        System.out.println("You are at start of playlist");
+                        isForward=true;
+                    }
+                    break;
+                case 3:
+                    if(isForward==true){
+                        if(itr.hasNext()){
+                            System.out.println(itr.next());
+                            isForward=false;
+                        }
+                    }
+                    else {
+                        if(itr.hasNext()){
+                            System.out.println(itr.next());
+                            isForward=true;
+                        }
+                    }
+                    break;
+                case 4:
+                    printMenu();
+                    break;
+                case 5:
+                    if(isForward==true){
+                        itr.remove();
+                        System.out.println("song is deleted successfully");
+                    }
+                    else{
+                        itr.remove();
+                        System.out.println("Song is deleted successfully");
+                        isForward=true;
+                    }
+                    break;
+                case 6:
+                    printSongs(playList);
+                    break;
+                case 7:
+                    quite=true;
+                    break;
+
+            }
+        }
+    }
+
+    public static void printSongs(LinkedList<Song>playList){
+        for (Song song:playList)
+            System.out.println(song);
+        return;
+    }
+    public static void printMenu(){
+        System.out.println("Select your choice:");
+        System.out.println("1-Play next song");
+        System.out.println("2-Play previous song");
+        System.out.println("3-Repeat the current song");
+        System.out.println("4-Show menu again");
+        System.out.println("5-Delete the current song");
+        System.out.println("6-Print all the songs in PlayList");
+        System.out.println("7-Quit");
+    }
+
+
 }
